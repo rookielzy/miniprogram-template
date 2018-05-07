d4e583a467299fde27e5418e12a05e5cb11c1b36
rookielzy <slimjetlzy@gmail.com>
2018-05-07 21:57:16 +0800

    Init

---
+++ components/carousel/carousel.js
@@ -0,0 +1,66 @@
+Component({
+
+  behaviors: [],
+
+  properties: {
+    myProperty: { // 属性名
+      type: String, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
+      value: '', // 属性初始值（可选），如果未指定则会根据类型选择一个
+      observer: function(newVal, oldVal){} // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串, 如：'_propertyChange'
+    },
+    myProperty2: String // 简化的定义方式
+  },
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  
+  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+  attached: function(){},
+  moved: function(){},
+  detached: function(){},
+
+  methods: {
+    onMyButtonTap: function(){
+      this.setData({
+        // 更新属性和数据的方法与更新页面数据的方法类似
+      })
+    },
+    _myPrivateMethod: function(){
+      // 内部方法建议以下划线开头
+      this.replaceDataOnPath(['A', 0, 'B'], 'myPrivateData') // 这里将 data.A[0].B 设为 'myPrivateData'
+      this.applyDataUpdates()
+    },
+    _propertyChange: function(newVal, oldVal) {
+
+    },
+    changeIndicatorDots: function(e) {
+      this.setData({
+        indicatorDots: !this.data.indicatorDots
+      })
+    },
+    changeAutoplay: function(e) {
+      this.setData({
+        autoplay: !this.data.autoplay
+      })
+    },
+    intervalChange: function(e) {
+      this.setData({
+        interval: e.detail.value
+      })
+    },
+    durationChange: function(e) {
+      this.setData({
+        duration: e.detail.value
+      })
+    }
+  }
+
+})
---
+++ utils/api.js
@@ -0,0 +1,22 @@
+/**
+ * 微信请求 Promise 化
+ * @param {String} type 请求类型
+ * @param {String} url 请求URL
+ * @param {Object} data 请求携带的数据
+ */
+function wxRequest (type = 'GET', url, data = {}) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      method: type,
+      data,
+      success: (res) => {
+        // TODO: 添加错误码处理
+        resolve(res)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    })
+  })
+}
