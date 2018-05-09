88e0a828526d02b17f1002090b618f3bca4958da
rookielzy <slimjetlzy@gmail.com>
2018-05-09 13:38:37 +0800

    Fixed Render API

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,24 +8,24 @@ Page({
   },
 
   onLoad: function () {
-    wx.login({
-      success: (res) => {
-        const code = res.code
-        console.log('code', code)
-        wx.getExtConfig({
-          success: (res) => {
-            const { appid } = res.extConfig
+    // wx.login({
+    //   success: (res) => {
+    //     const code = res.code
+    //     console.log('code', code)
+    //     wx.getExtConfig({
+    //       success: (res) => {
+    //         const { appid } = res.extConfig
 
-            wxLogin({
-              "code": code,
-              "appid": appid
-            }).then(res => {
-              console.log('test :', res)
-            })
-          }
-        })
-      }
-    })
+    //         wxLogin({
+    //           "code": code,
+    //           "appid": appid
+    //         }).then(res => {
+    //           console.log('test :', res)
+    //         })
+    //       }
+    //     })
+    //   }
+    // })
 
     wxRender().then(res => {
       const renderData = res.data.data
